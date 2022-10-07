4edc4b84c98e718b6b88a1c6edcc41f01040a1e2
harukino00 <ddoanthanhhuy@gmail.com>
2022-10-08 00:04:32 +0700

    07/10/2022 12:04AM update

--- src/index.js
+++ src/index.js
@@ -7,8 +7,13 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname,'public')));
 
+app.use(express.urlencoded({
+  extended:true
+}));
+app.use(express.json());
+
 //HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 //Template engine
 app.engine('hbs', engine({
@@ -22,12 +27,19 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+// app.get('/news', (req, res) => {
+//   res.render('news');
+// })
 
 app.get('/search', (req, res) => {
   res.render('search');
+})
+
+app.post('/search', (req, res) => {
+
+  console.log(req.body);
+
+  res.send('');
 })
 
 app.listen(port, () => {
