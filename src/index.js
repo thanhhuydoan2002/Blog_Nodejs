87d05900f56def20509d39634620f41300b1987b
harukino00 <ddoanthanhhuy@gmail.com>
2022-10-07 22:29:13 +0700

    07/10/2022 update

--- src/index.js
+++ src/index.js
@@ -26,6 +26,10 @@ app.get('/news', (req, res) => {
   res.render('news');
 })
 
+app.get('/search', (req, res) => {
+  res.render('search');
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
 })
