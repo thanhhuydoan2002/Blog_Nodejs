562c7edc1d3a4a29be220fb7720ee4233e0a9ffd
doanthanhhuy <ddoanthanhhuy@gmail.com>
2022-10-11 23:44:26 +0700

    test husky

--- src/index.js
+++ src/index.js
@@ -18,19 +18,18 @@ app.use(express.json());
 // app.use(morgan('combined'));
 
 //Template engine
-          app.engine(
+app.engine(
     'hbs',
     engine({
         extname: '.hbs',
     }),
 );
-                  app.set('view engine', 'hbs');
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 //Route inits
 route(app);
 
-            app.listen(port, () => {
-
+app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`);
 });
